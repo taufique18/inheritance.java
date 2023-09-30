cc9e1c6d812f937b0f188df6bd4b790d2b183b50
taufique18 <125634395+taufique18@users.noreply.github.com>
2023-09-30 06:14:47 -0700

    Add files via upload

---
+++ inheritance/Main.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Main extends Employee {
+
+	public Main(String name, int empId) {
+		super(name, empId);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		Manager m1 = new Manager("Taufique", 1001,"B.Tech");
+        m1.display();
+              
+        Technician t1 = new Technician("Mohammed", 1002, "Java");
+        t1.display();
+	}
+
+}
